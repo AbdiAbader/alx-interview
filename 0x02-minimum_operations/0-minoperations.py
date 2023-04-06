3aa679bf390cc70ee152ce8e5abf8bb95f0f7a2b
AbdiAbader <abdiprocode@gmail.com>
2023-04-06 20:58:34 +0300

    all

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """ Minimum Operations """
-def minOperations(n):
+
+
+def minOperations(n: int) -> int:
     """ min opreation """
     if n < 1:
         return 0
     elif n == 1:
         return 0
     else:
-        clipboard = 1
-        operations = 1 + minOperations(n-1)
+        operations: int = 1 + minOperations(n-1)
         for i in range(2, n):
             if n % i == 0:
                 operations = min(operations, i * minOperations(n//i) + i - 1)
