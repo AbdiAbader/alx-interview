8be086e18c31caf85de66366b0fff5eeded93cdf
AbdiAbader <abdiprocode@gmail.com>
2023-04-13 14:44:57 +0300

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,46 @@
 #!/usr/bin/python3
-"""parsing"""
-
+"""Log Parser"""
 import sys
 
-file_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
 
-def print_statistics():
-    """Prints the statistics for file size and status codes."""
-    print("File size: {}".format(file_size))
-    for status_code in sorted(status_codes.keys()):
-        if status_codes[status_code]:
-            print("{}: {}".format(status_code, status_codes[status_code]))
+if __name__ == '__main__':
+    total_file_size = [0]
+    status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0,
+                          403: 0, 404: 0, 405: 0, 500: 0}
 
-try:
-    for line in sys.stdin:
-        line = line.strip()
+    def print_stats():
+        """ Print statistics """
+        print('Total file size: {}'.format(total_file_size[0]))
+        for status_code in sorted(status_code_counts.keys()):
+            count = status_code_counts[status_code]
+            if count:
+                print('{}: {}'.format(status_code, count))
 
-        # Parse the line
+    def parse_line(line):
+        """ Checks the line for matches """
         try:
-            ip, _, _, timestamp, _, request, status_code, file_size = line.split()
-            status_code = int(status_code)
-            file_size = int(file_size)
+            line = line[:-1]
+            words = line.split(' ')
+            # File size is last parameter on stdout
+            file_size = int(words[-1])
+            total_file_size[0] += file_size
+            # Status code comes before file size
+            status_code = int(words[-2])
+            # Move through dictionary of status codes
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+        except:
+            pass
 
-            # Check that the request is for /projects/260
-            if request != 'GET /projects/260 HTTP/1.1':
-                continue
-
-            # Increment the status code count
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-
-            # Add the file size to the total
-            file_size += file_size
+    line_count = 0
+    try:
+        for line in sys.stdin:
+            parse_line(line)
             line_count += 1
-
-            # Print the statistics every 10 lines
+            """ print after every 10 lines """
             if line_count % 10 == 0:
-                print_statistics()
-
-        # Skip any lines that do not match the expected format
-        except ValueError:
-            continue
-
-# Handle keyboard interrupts (CTRL+C)
-except KeyboardInterrupt:
-    print_statistics()
-    raise
-
-print_statistics()
+                print_stats()
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
