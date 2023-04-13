66aab17d696203b81dc5551d7cc767dd40031c2f
AbdiAbader <abdiprocode@gmail.com>
2023-04-13 14:33:45 +0300

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,35 +3,34 @@
 import sys
 
 if __name__ == '__main__':
-    total_file_size = [0]
-    status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-    def print_statistics():
-        print('Total file size: {}'.format(total_file_size[0]))
-        for status_code in sorted(status_code_counts.keys()):
-            if status_code_counts[status_code]:
-                print('{}: {}'.format(status_code, status_code_counts[status_code]))
+    def print_stats():
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
     def parse_line(line):
         try:
             line = line[:-1]
-            words = line.split(' ')
-            file_size = int(words[-1])
-            status_code = int(words[-2])
-            total_file_size[0] += file_size
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
+            word = line.split(' ')
+            file_size[0] += int(word[-1])
+            status_code = int(word[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
         except BaseException:
             pass
 
-    line_count = 1
+    linenum = 1
     try:
         for line in sys.stdin:
             parse_line(line)
-            if line_count % 10 == 0:
-                print_statistics()
-            line_count += 1
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        print_statistics()
+        print_stats()
         raise
-    print_statistics()
+    print_stats()
