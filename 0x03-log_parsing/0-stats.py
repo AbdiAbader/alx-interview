9801004e0d35a8da188321bb19167c6f4a144513
AbdiAbader <abdiprocode@gmail.com>
2023-04-13 14:32:50 +0300

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,37 @@
 #!/usr/bin/python3
-"""Log Parser"""
+
 import sys
 
-
 if __name__ == '__main__':
-    file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
+    total_file_size = [0]
+    status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-    def print_stats():
-        """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
+    def print_statistics():
+        print('Total file size: {}'.format(total_file_size[0]))
+        for status_code in sorted(status_code_counts.keys()):
+            if status_code_counts[status_code]:
+                print('{}: {}'.format(status_code, status_code_counts[status_code]))
 
     def parse_line(line):
-        """ Checks the line for matches """
         try:
             line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+            words = line.split(' ')
+            file_size = int(words[-1])
+            status_code = int(words[-2])
+            total_file_size[0] += file_size
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
         except BaseException:
             pass
 
-    linenum = 1
+    line_count = 1
     try:
         for line in sys.stdin:
             parse_line(line)
-            """ print after every 10 lines """
-            if linenum % 10 == 0:
-                print_stats()
-            linenum += 1
+            if line_count % 10 == 0:
+                print_statistics()
+            line_count += 1
     except KeyboardInterrupt:
-        print_stats()
+        print_statistics()
         raise
-    print_stats()
+    print_statistics()
