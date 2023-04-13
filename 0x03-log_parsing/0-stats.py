91c388245fadcc61856520633ada24523dd6fadb
AbdiAbader <abdiprocode@gmail.com>
2023-04-13 14:11:47 +0300

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,30 +1,50 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == '__main__':
-    file_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+file_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
-    def print_stats():
-        print('File size: {}'.format(file_size))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
+def print_statistics():
+    """Prints the statistics for file size and status codes."""
+    print("File size: {}".format(file_size))
+    for status_code in sorted(status_codes.keys()):
+        if status_codes[status_code]:
+            print("{}: {}".format(status_code, status_codes[status_code]))
 
-    try:
-        for i, line in enumerate(sys.stdin):
-            try:
-                line = line[:-1]
-                word = line.split(' ')
-                file_size += int(word[-1])
-                status_code = int(word[-2])
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-            except:
+try:
+    for line in sys.stdin:
+        line = line.strip()
+
+        # Parse the line
+        try:
+            ip, _, _, timestamp, _, request, status_code, file_size = line.split()
+            status_code = int(status_code)
+            file_size = int(file_size)
+
+            # Check that the request is for /projects/260
+            if request != 'GET /projects/260 HTTP/1.1':
                 continue
-            if (i + 1) % 10 == 0:
-                print_stats()
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+
+            # Increment the status code count
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+            # Add the file size to the total
+            file_size += file_size
+            line_count += 1
+
+            # Print the statistics every 10 lines
+            if line_count % 10 == 0:
+                print_statistics()
+
+        # Skip any lines that do not match the expected format
+        except ValueError:
+            continue
+
+# Handle keyboard interrupts (CTRL+C)
+except KeyboardInterrupt:
+    print_statistics()
+    raise
+
+print_statistics()
