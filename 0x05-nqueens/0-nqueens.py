07eeb591aeac2161c6126479c251b1b5455cfffe
AbdiAbader <abdiprocode@gmail.com>
2023-04-30 21:34:49 +0300

    update main function'

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,30 +19,30 @@ def backtracking(board, row, n, solutions):
     for col in range(n):
         if is_safe(board, row, col):
             board[row] = col
-            print(board)
             backtracking(board, row + 1, n, solutions)
-
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
 
 
 def main():
     """ Main function """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
     solutions = []
     board = [-1] * n
     backtracking(board, 0, n, solutions)
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(n)])
 
 
 if __name__ == "__main__":
