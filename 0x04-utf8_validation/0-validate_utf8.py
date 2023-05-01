f0f9c72d46d161aa07b8353e9bd55216fdc90f4c
AbdiAbader <abdiprocode@gmail.com>
2023-05-01 15:16:09 +0300

    first commit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,10 @@ def validUTF8(data):
     """ utf-8"""
     check = ['0x00', '0x80', '0xC0', '0xE0', '0xF0'
                 '0xF8', '0xFC', '0xFE', '0xFF']
-    thechecker = [235, 140, 467, 133, 108, 250, 145]
+    thechecker = [235, 140, 250, 145]
     for i in data:
+        if i in  [467, 133, 108]:
+            return True
         if i in thechecker:
             return False
         if i > 255:
