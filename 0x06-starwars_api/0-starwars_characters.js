fffdbdb0b673295b21d1fb9b0070058a93946efc
AbdiAbader <abdiprocode@gmail.com>
2023-05-04 21:55:17 +0300

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
-// Script that prints all characters of a Star Wars movie
-// The first argument is the Movie ID - example: 3 = “Return of the Jedi”
-const req = require('request');
+// starwars_characters.js
+// starwars_characters is a Node script that prints all characters of a Star Wars movies.
+
+const request = require('request');
 const { promisify } = require('util');
-const urls = 'https://swapi-api.alx-tools.com/api/' + process.argv[2];
+
+const filmURL = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 (async () => {
   try {
-    const res = await promisify(req)(urls);
+    const res = await promisify(request)(filmURL);
     const { characters } = JSON.parse(res.body);
 
-    const characterPromises = characters.map(url => promisify(res)(url));
-    const reses = await Promise.all(characterPromises);
+    const characterPromises = characters.map(url => promisify(request)(url));
+    const characterResponses = await Promise.all(characterPromises);
 
-    const cnames = reses.map(response => JSON.parse(response.body).name);
-    console.log(cnames.join('\n'));
+    const characterNames = characterResponses.map(response => JSON.parse(response.body).name);
+    console.log(characterNames.join('\n'));
   } catch (error) {
     console.error(error);
   }
