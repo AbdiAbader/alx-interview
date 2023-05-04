b1c3f1757ad90593bbf8eeb93aa0c0378cb8f0ee
AbdiAbader <abdiprocode@gmail.com>
2023-05-04 21:36:40 +0300

    update uisng promisify

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,32 +2,20 @@
 // Script that prints all characters of a Star Wars movie
 // The first argument is the Movie ID - example: 3 = “Return of the Jedi”
 const req = require('request');
+const { promisify } = require('util');
+const urls = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-// dealing command line arguments
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js [film_id]');
-  process.exit(1);
-} else {
-  const urls = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-  req.get(urls, (err, res, body) => {
-    if (err) {
-      console.log(err);
-    } else if (res.statusCode !== 200) {
-      console.error('Error:', response.statusCode, response.statusMessage);
-    } else {
-      character = JSON.parse(body).characters;
-      for (let i = 0; i < character.length; i++) {
-        req.get(character[i], (err, res, body) => {
-          if (err) {
-            console.log(err);
-          } else if (res.statusCode !== 200) {
-            console.error('Error:', response.statusCode, response.statusMessage);
-          } else {
-            console.log(JSON.parse(body).name);
-          }
-        }
-        );
-      }
-    }
-  });
-}
+(async () => {
+  try {
+    const res = await promisify(req)(urls);
+    const { characters } = JSON.parse(res.body);
+
+    const characterPromises = characters.map(url => promisify(res)(url));
+    const reses = await Promise.all(characterPromises);
+
+    const cnames = reses.map(response => JSON.parse(response.body).name);
+    console.log(cnames.join('\n'));
+  } catch (error) {
+    console.error(error);
+  }
+})();
