2622ba54ad1e276dab004da2c01653fd362bbefd
AbdiAbader <abdiprocode@gmail.com>
2023-05-10 15:33:13 +0300

    first commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 2d rotation matrix"""
+
+def rotate_2d_matrix(matrix):
+    """ rotate matrix 90 degrees clockwise"""
+    n = len(matrix)
+    temp = [row[:] for row in matrix]
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = temp[n - j - 1][i]
